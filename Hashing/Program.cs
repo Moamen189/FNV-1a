b9730c75607e2dcfbba2a6f986531cde1b45d9bc
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-08 22:06:38 +0200

    Add project files.

---
+++ Hashing/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hashing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash hash = new Hash();
+            string x = "Moamen Ashraf Muhhhamed Ali";
+            var y =  hash.Hash32(x);
+            var z = hash.Hash64(x);
+            Console.WriteLine("Name: " + x );
+            Console.WriteLine("Hash 32 By FNV-1a: " + y);
+            Console.WriteLine("Hash 64 By FNV-1a: " + z);
+        }
+    }
+}
